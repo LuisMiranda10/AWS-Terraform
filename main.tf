150de701b03036bd42383c47ed1af49b52b93c87
LuisMiranda10 <luiseduardocarneiro6@gmail.com>
2024-08-16 01:27:56 -0300

    hosting a static website

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "${var.bucket_prefix}-terraform-s3"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.mybucket,
+    aws_s3_bucket_public_access_block.mybucket,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "bucket_prefix" {
+  type        = string
+  description = "A prefix applied to the S3 bucket created to ensure a unique name."
+  default = "mybuck"
+}
+
+
